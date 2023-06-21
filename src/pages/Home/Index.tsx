692cb00f8ead4ad5ba76620f0c6be4a25764328f
FeBotero <frpbotero@gmail.com>
2023-06-21 12:11:09 -0400

    update Home Page styles

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,26 +1,43 @@
-import { HomeContainer } from "./style";
+import { HomeContainer, HomeContent, ProductContainer } from "./style";
 import coffeeHome from "../../assets/coffeHome.svg"
+import IconBox from "../../assets/IconBox.svg"
+import IconCar from "../../assets/IconCar.svg"
+import IconCoffee from "../../assets/IconCoffee.svg"
+import IconTime from "../../assets/IconTime.svg"
+
+
+
+
 
 export function Home() {
   return (
     <HomeContainer>
-      <div>
-        <div>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <p>Com o Coffee Delivery você recebbe seu café onde estiver, a qualquer hora</p>
+      <HomeContent>
+        
+        <div className="content">
+          <div className="promCoffee">
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>Com o Coffee Delivery você recebbe seu café onde estiver, a qualquer hora</p>
+          </div>
+          <div className="promDelivery">
+            <p><img src={IconCar} alt="BuyCar" /> Compra simples e segura</p>
+            <p><img src={IconBox} alt="Box" /> Embalagem mantém o café intacto</p>
+            <p><img src={IconTime} alt="Time" /> Entrega rápida e rastreada</p>
+            <p><img src={IconCoffee} alt="Coffee" /> O café chega fresquino até você</p>
+          </div>
         </div>
         <div>
-          <p>Compra simples e segura</p>
-          <p>Embalagem mantém o café intacto</p>
-          <p>Entrega rápida e rastreada</p>
-          <p>O café chega fresquino até você</p>
+          <img src={coffeeHome} alt="" />
         </div>
-      </div>
-      <div>
-        <img src={coffeeHome} alt="" />
-      </div>
+      </HomeContent>
+      <ProductContainer>
+        <div>
+          <h3>Nossos Cafés</h3>
+        </div>
+        <div className="products">
 
+        </div>
+      </ProductContainer>
     </HomeContainer>
-
   )
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,7 +1,60 @@
 import styled from "styled-components";
 
-
 export const HomeContainer=styled.main`
-  background-image: url("/src/assets/Background.svg");
+  display: flex;
+  flex-direction: column;
 
+`
+
+
+export const HomeContent=styled.div`
+  background-image: url("/src/assets/Background.svg");
+  background-size: 100vw;
+  
+  display: flex;
+  justify-content: space-between;
+  padding: 6rem;
+
+  
+
+  .content{
+    display: flex;
+    
+    flex-direction: column;
+    
+  }
+  .promCoffee{
+    display: flex;
+    flex-direction: column;
+
+    h1{
+      font-size: 4rem;
+      color: ${props=>props.theme["base-title"]};
+    }
+    p{
+      font-size:1.5rem;
+      color: ${props=>props.theme["base-subtitle"]};
+    }
+  }
+  .promDelivery{
+    display: grid;
+    grid-template-rows: repeat(2,1fr);
+    grid-template-columns: repeat(2,1fr);
+    grid-row-gap: 1rem;
+    margin-top: 4rem;
+    grid-column-gap: 1rem;
+    color: ${props=>props.theme["base-text"]};
+
+    p{
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+  }
+
+`
+export const ProductContainer=styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 6rem;
 `
