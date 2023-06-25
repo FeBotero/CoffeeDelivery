b4cc12b0a9a57c18ca068b4699a8c9be31380f74
FeBotero <frpbotero@gmail.com>
2023-06-24 21:26:26 -0400

    Include ContexteProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 import { Router } from "./Router"
 import { BrowserRouter } from "react-router-dom"
+import { ProductsContextProvider } from "./contexts/ProductContext"
 
 function App() {
-  
+
 
   return (
-    
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-        <Router/>
-        </BrowserRouter>
-        <GlobalStyle/>
-      </ThemeProvider>
+
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <ProductsContextProvider>
+          <Router />
+        </ProductsContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
